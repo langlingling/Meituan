6cb308fac39962994f48ba24baa7916ce0d65620
langlingling <2530752882@qq.com>
2018-12-28 19:13:31 +0800

    郎玲玲头部

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,8 @@ var sass = require("gulp-sass");
 gulp.task("sass", function() {
     return gulp.src("src/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("dist/"))
+        .pipe(gulp.dest("src/dist/"))
+})
+gulp.task("watch", function() {
+    gulp.watch("src/scss/*.scss", gulp.series("sass"))
 })
